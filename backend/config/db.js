80066b90503fa8f632c810c415f4473fc44a65e0
Sanjay Mansel <mansel@Sanjays-MacBook-Air.local>
2023-05-05 14:08:39 +0900

    First Look of Movie Review Page

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,22 +3,30 @@ import dontenv from "dotenv";
 
 dontenv.config();
 
-const dbConn = mysql.createConnection({
+const env = process.env.NODE_ENV;
+
+const dbConnProd = mysql.createPool({
+  connectionLimit: 10,
+  acquireTimeout: 10000,
   host: process.env.DB_HOST,
-  user: "root",
+  user: process.env.DB_USER,
   password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
 });
 
-try {
-  dbConn.connect((err) => {
-    if (err) {
-      console.log(`Error - , ${err.message}`.red.bold);
-    } else {
-      console.log(`Database Connected!`.underline.cyan);
-    }
-  });
-} catch (error) {
-  console.log(`Error - , ${error.message}`.red.bold);
+let dbConn = mysql.createPool({
+  connectionLimit: 10,
+  acquireTimeout: 10000,
+  host: process.env.LOCAL_DB_HOST,
+  user: process.env.LOCAL_DB_USER,
+  password: process.env.LOCAL_DB_PWD,
+  database: process.env.LOCAL_DB_NAME,
+  multipleStatements: true,
+});
+
+if (env === "prod") {
+  dbConn = dbConnProd;
 }
 
 export default dbConn;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import classNames from "classnames";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../actions/userAction";
+import redIcon from "../images/SeaWitchRed.svg";
 
 const Header = () => {
+  const [floating, setFloating] = useState(false);
+
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
@@ -15,20 +19,51 @@ const Header = () => {
     dispatch(logout());
   };
 
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 100) {
+        setFloating(true);
+      } else {
+        setFloating(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar
+        className={classNames("navbar navbar-expand-lg navbar-dark header", {
+          "header--float": floating,
+        })}
+        expand="lg"
+        collapseOnSelect
+        fixed="top"
+      >
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>Template</Navbar.Brand>
+            <Navbar.Brand>
+              <img src={redIcon} alt="sea" className="header__logo" />
+              <span className="header__name">Sea Witch</span>
+            </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <LinkContainer to="/admin">
                 <Nav.Link>
-                  <i className="fa fa-bell px-1" aria-hidden="true" />
-                  Admin
+                  <i className="fa fa-list px-1" aria-hidden="true" />
+                  Watchlist
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/admin">
+                <Nav.Link>
+                  <i className="fa fa-film px-1" aria-hidden="true" />
+                  My Reviews
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
